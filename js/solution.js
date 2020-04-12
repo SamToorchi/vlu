e155f8e0a43b6c046810814f17ba1376151b5dc3
SamToorchi <stoorchi@googlemail.com>
2020-04-12 22:19:57 +0200

    kon ready

--- js/solution.js
+++ js/solution.js
@@ -1,7 +1,6 @@
 document.getElementById("solution_area").style.display = "none";
 
 function showHtmlCode() {
-     console.log(document.getElementById("solution_area").style.display);
      if (document.getElementById("solution_area").style.display == "none") {
          document.getElementById("solution_area").style.display = "block";
      } else {
